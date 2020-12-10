ac1f9568828659acff85f5bb74dd791b5e7e5beb
Persephone Walker <seph.walker@gwarhammer.co.uk>
2020-12-10 09:59:01 +0000

    I rewrote it so it didn't take a millionty billionty years to run.

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -31,9 +31,9 @@ def find_next_options(adapters, value):
 
 def find_arrangements(adapters, arrangements):
     """
-    >>> len(find_arrangements([i for i in reversed(sorted([16,10,15,5,1,11,7,19,6,12,4]))],[[22]]))
+    #>>> len(find_arrangements([i for i in reversed(sorted([16,10,15,5,1,11,7,19,6,12,4]))],[[22]]))
     8
-    >>> len(find_arrangements([i for i in reversed(sorted([28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]))],[[52]]))
+    #>>> len(find_arrangements([i for i in reversed(sorted([28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]))],[[52]]))
     19208
     """
     arrs = []
@@ -59,6 +59,27 @@ def sort_and_reverse(lst):
 def file_to_ratings(fname):
     return [int(line.strip()) for line in open(fname,"r")]
 
+
+def build_map(adapters, mapping, cur_val=0):
+    target = max(adapters)+3
+    if cur_val in mapping or cur_val==target:
+        return
+    mapping[cur_val] = [j for j in sorted([i for i in adapters if i-cur_val <= 3 and i-cur_val > 0])]
+    if target-cur_val <= 3 and target-cur_val > 0:
+        mapping[cur_val].append(target)
+    for i in mapping[cur_val]:
+        build_map(adapters,mapping,i)
+
+def adapter_to_count(mapping,val,cache):
+    if val not in cache:
+        if val not in mapping: # This is our target
+            return 1
+        if len(mapping[val]) == 0:
+            cache[val] = 1
+        else:
+            cache[val] = sum([adapter_to_count(mapping,i,cache) for i in mapping[val]])
+    return cache[val]
+
 if __name__=='__main__':
     import doctest
     doctest.testmod()
@@ -66,4 +87,9 @@ if __name__=='__main__':
     adapters = file_to_ratings("2020/day10/input.txt")
     diffs = adapter_differences(adapters)
     print(diffs[0]*diffs[1])
-    print(len(find_arrangements(sort_and_reverse(adapters),[max(adapters)+3])))
+
+    cache = {}
+    mapping = {}
+    build_map(adapters, mapping)
+    adapter_to_count(mapping,0,cache)
+    print(cache[0])
--- 2020/day10/day10_test.py
+++ 2020/day10/day10_test.py
@@ -3,27 +3,39 @@ import unittest
 import day10
 
 class TestAdapters(unittest.TestCase):
-    def test_three_in_list(self):
-        adapters = day10.sort_and_reverse([1,2,3,4])
-        arrangements = day10.find_arrangements(adapters,[[max(adapters)+3]])
-        self.assertIn([7,4,3,2,1],arrangements)
-        self.assertIn([7,4,3,2],arrangements)
-        self.assertIn([7,4,3],arrangements)
+    def test_build_map(self):
+        adapters = [16,10,15,5,1,11,7,19,6,12,4]
+        mapping = {}
+        day10.build_map(adapters,mapping)
+        expect = {0: [1],  1: [4],  4: [5, 6, 7],  5: [6, 7],  6: [7],  7: [10],  10: [11, 12],  11: [12],  12: [15],  15: [16],  16: [19],  19: [22]}
+        self.assertEqual(mapping,expect)
+
+    def test_build_map_large(self):
+        adapters = [28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]
+        mapping = {}
+        day10.build_map(adapters,mapping)
+        expect = {0: [1, 2, 3],  1: [2, 3, 4],  2: [3, 4],  3: [4],  4: [7],  7: [8, 9, 10],  8: [9, 10, 11],  9: [10, 11],  10: [11],  11: [14],  14: [17],  17: [18, 19, 20],  18: [19, 20],  19: [20],  20: [23],  23: [24, 25],  24: [25],  25: [28],  28: [31],  31: [32, 33, 34],  32: [33, 34, 35],  33: [34, 35],  34: [35],  35: [38],  38: [39],  39: [42],  42: [45],  45: [46, 47, 48],  46: [47, 48, 49],  47: [48, 49],  48: [49],  49: [52]}
+        self.assertEqual(mapping,expect)
+
+    def test_count(self):
+        adapters = [16,10,15,5,1,11,7,19,6,12,4]
+        mapping = {}
+        day10.build_map(adapters,mapping)
+        cache = {}
+        day10.adapter_to_count(mapping,0,cache)
+        self.assertEqual(cache[0],8)
+        expect = {0: 8, 1: 8, 4: 8, 5: 4, 6: 2, 7: 2, 10: 2, 11: 1, 12: 1, 15: 1, 16: 1, 19: 1}
+        self.assertEqual(cache,expect)
+
+    def test_count_large(self):
+        adapters = [28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]
+        mapping = {}
+        day10.build_map(adapters,mapping)
+        cache = {}
+        day10.adapter_to_count(mapping,0,cache)
+        self.assertEqual(cache[0],19208)
         
-    def test_arrangements_large(self):
-        adapters = day10.sort_and_reverse([28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3])
-        arrangements = day10.find_arrangements(adapters,[[max(adapters)+3]])
-        self.assertIn(day10.sort_and_reverse([1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31, 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31, 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31, 32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31, 32, 33, 34, 35, 38, 39, 42, 45, 46, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31, 32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45, 46, 48, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45, 46, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45, 47, 48, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45, 47, 49, 52]),arrangements)
-        self.assertIn(day10.sort_and_reverse([3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45, 48, 49, 52]),arrangements)
-        self.assertEqual(len(arrangements),19208)
+
 
 if __name__=='__main__':
     unittest.main()
